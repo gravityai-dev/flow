aba01c9064018ef405dff5708a46ad470cfb234c
Gavin Payne <gavinapayne@gmail.com>
2025-09-11 15:54:34 +0400

    v1.0.5: version fix

--- src/Loop/index.ts
+++ src/Loop/index.ts
@@ -14,6 +14,7 @@ export function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
+    packageVersion: "1.0.4",
     type: NODE_TYPE,
   name: "Loop",
   description: "Iterate through array items one by one",
