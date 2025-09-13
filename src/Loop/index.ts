9428be7f4a703a87a15bd7b8e707fdd0a693c4b5
Gavin Payne <gavinapayne@gmail.com>
2025-09-13 17:08:15 +0400

    v1.0.9

--- src/Loop/index.ts
+++ src/Loop/index.ts
@@ -14,7 +14,7 @@ export function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
-    packageVersion: "1.0.5",
+    packageVersion: "1.0.8",
     type: NODE_TYPE,
   name: "Loop",
   description: "Iterate through array items one by one",
