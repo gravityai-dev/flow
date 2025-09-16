5a77773eeb3bc4c790d3a4cec2c492252b09c476
Gavin Payne <gavinapayne@gmail.com>
2025-09-16 17:58:04 +0400

    v1.0.13

--- src/Note/index.ts
+++ src/Note/index.ts
@@ -14,7 +14,7 @@ export function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
-    packageVersion: "1.0.11",
+    packageVersion: "1.0.12",
     type: NODE_TYPE,
   isService: false,
   name: "Note",
