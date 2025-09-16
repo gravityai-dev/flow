92360aa3021c5cc407af9a4b726b761b2c94d829
Gavin Payne <gavinapayne@gmail.com>
2025-09-16 20:27:00 +0400

    v1.0.14

--- src/UMAP/index.ts
+++ src/UMAP/index.ts
@@ -14,7 +14,7 @@ export function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
-    packageVersion: "1.0.12",
+    packageVersion: "1.0.13",
     type: NODE_TYPE,
   isService: false,
   name: "UMAP",
