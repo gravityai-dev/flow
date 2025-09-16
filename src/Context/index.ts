57eac8d3dceba9ebc5faff4f4cf2bae98412f153
Gavin Payne <gavinapayne@gmail.com>
2025-09-16 21:16:42 +0400

    v1.0.15

--- src/Context/index.ts
+++ src/Context/index.ts
@@ -14,7 +14,7 @@ export function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
-    packageVersion: "1.0.13",
+    packageVersion: "1.0.14",
     type: NODE_TYPE,
     isService: false,
     name: "Context",
